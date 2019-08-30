620dff646930945de6384218ab1f7c5c7a807547
nipun510 <nipunkumar510@gmail.com>
2019-08-30 10:55:54 +0000

    key file

---
+++ config_twitter.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'hrETgQm8IL9O3H1n3Z44XNsM2',
+    consumer_secret: 'jJt7oKx4Uiu5O5ikFFJ4kgic6XYJZece9JRdaDElbLEwPdLZEi',
+    access_token_key: '2403028304-ML9ygVFMRVTZRmT7dpjyHICwc60ilDEvvHnfnPV',
+    access_token_secret: 'CIHj8198WXws7PEtBPEc37WN2Xm6v1VD5Rfdhn2oCmeO6'
+}
